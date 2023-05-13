742f783b7dcf8f3df5fa88bce16ae61bb5d36c96
Samsung <arslan.zakirov.1996@mail.ru>
2023-05-13 15:38:16 +0300

    В коде используется JPA, получение студентов старше 18

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,34 @@
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("myPersistenceUnit");
+
+        EntityManagerFactory entityManagerFactory = Persistence.
+                createEntityManagerFactory("myPersistenceUnit");
+
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         entityManager.getTransaction().begin();
 
-        String jpqlQuery = "SELECT s FROM Student s WHERE s.age>: minAge";
+        String jpqlQuery = "SELECT s FROM Student s WHERE s.age > :minAge";
         TypedQuery<Student> query = entityManager.createQuery(jpqlQuery, Student.class);
-
         query.setParameter("minAge", 18);
-        List<Student> students = query.getResultList();
+        List<Student> studentList = query.getResultList();
 
         entityManager.getTransaction().commit();
 
-        for (Student student : students) {
+        for (Student student : studentList) {
             System.out.println("Student ID: " + student.getId());
             System.out.println("Student Name: " + student.getName());
             System.out.println("Student Age: " + student.getAge());
             System.out.println("------------");
         }
+
         entityManager.close();
         entityManagerFactory.close();
+
+
     }
 }
